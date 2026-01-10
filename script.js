a992bc9479edba1c6e70544ffa897ed3386f3468
Rakhul <rakhulsankar@gmail.com>
2026-01-10 19:14:01 +0530

    modified script

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var tablinks = document.getElementsByClassName("tab-links"); 
 var tabcontents = document.getElementsByClassName("tab-contents");
 
@@ -23,31 +22,28 @@ function closemenu(){
     sidemenu.style.right = "-200px";
 }
 
+  function showTab(tab) {
+    const skills = document.getElementById("skills");
+    const education = document.getElementById("education");
 
-=======
-var tablinks = document.getElementsByClassName("tab-links"); 
-var tabcontents = document.getElementsByClassName("tab-contents");
+    const skillsBtn = document.getElementById("skills-btn");
+    const educationBtn = document.getElementById("education-btn");
 
-function opentab(tabname){
-    for(tablink of tablinks){
-        tablink.classList.remove("active-link");
+    if (tab === "skills") {
+      skills.classList.remove("hidden");
+      education.classList.add("hidden");
+
+      skillsBtn.classList.add("border-b-2", "border-red-600");
+      educationBtn.classList.remove("border-b-2", "border-red-600");
+    } else {
+      education.classList.remove("hidden");
+      skills.classList.add("hidden");
+
+      educationBtn.classList.add("border-b-2", "border-red-600");
+      skillsBtn.classList.remove("border-b-2", "border-red-600");
     }
-    for(tabcontent of tabcontents){
-        tabcontent.classList.remove("active-tab");
-    }
-    event.currentTarget.classList.add("active-link");
-    document.getElementById(tabname).classList.add("active-tab");
-} 
+  }
 
-var sidemenu = document.getElementById("sidemenu");
-
-function openmenu(){
-    sidemenu.style.right = "0";
+function toggleMenu() {
+  document.getElementById("mobileMenu").classList.toggle("hidden");
 }
-
-function closemenu(){
-    sidemenu.style.right = "-200px";
-}
-
-
->>>>>>> 2a67795232b214d76060148448f2c13adb901a72
