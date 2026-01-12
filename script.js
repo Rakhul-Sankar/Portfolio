6da847d15651060a0225ff00da95284e901ddc52
Rakhul <rakhulsankar@gmail.com>
2026-01-12 11:08:13 +0530

    modified education

--- script.js
+++ script.js
@@ -1,31 +1,33 @@
+  // Mobile menu
   function toggleMenu() {
     document.getElementById("mobileMenu").classList.toggle("hidden");
   }
 
-  function showTab(tab) {
-    const skills = document.getElementById("skills");
-    const education = document.getElementById("education");
-    const skillsBtn = document.getElementById("skills-btn");
-    const educationBtn = document.getElementById("education-btn");
+  // About tabs
+  const skillsBtn = document.getElementById("skills-btn");
+  const educationBtn = document.getElementById("education-btn");
+  const skills = document.getElementById("skills");
+  const education = document.getElementById("education");
 
-    if (tab === "skills") {
-      skills.classList.remove("hidden");
-      education.classList.add("hidden");
+  skillsBtn.addEventListener("click", () => {
+    skills.classList.remove("hidden");
+    education.classList.add("hidden");
 
-      skillsBtn.classList.add("border-red-600");
-      skillsBtn.classList.remove("border-transparent", "text-gray-400");
+    skillsBtn.classList.add("border-red-600");
+    skillsBtn.classList.remove("text-gray-400");
 
-      educationBtn.classList.remove("border-red-600");
-      educationBtn.classList.add("border-transparent", "text-gray-400");
-    } else {
-      education.classList.remove("hidden");
-      skills.classList.add("hidden");
+    educationBtn.classList.remove("border-red-600");
+    educationBtn.classList.add("text-gray-400");
+  });
 
-      educationBtn.classList.add("border-red-600");
-      educationBtn.classList.remove("border-transparent", "text-gray-400");
+  educationBtn.addEventListener("click", () => {
+    education.classList.remove("hidden");
+    skills.classList.add("hidden");
 
-      skillsBtn.classList.remove("border-red-600");
-      skillsBtn.classList.add("border-transparent", "text-gray-400");
-    }
-  }
+    educationBtn.classList.add("border-red-600");
+    educationBtn.classList.remove("text-gray-400");
+
+    skillsBtn.classList.remove("border-red-600");
+    skillsBtn.classList.add("text-gray-400");
+  });
 
