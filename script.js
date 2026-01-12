637fa337382bd6470cf4eb0330215a64131bdeea
Rakhul <rakhulsankar@gmail.com>
2026-01-12 17:21:54 +0530

    added dark mode

--- script.js
+++ script.js
@@ -36,3 +36,28 @@ document.addEventListener("DOMContentLoaded", () => {
     setActive(educationBtn, skillsBtn, education, skills);
   });
 });
+
+const html = document.documentElement;
+  const themeIcon = document.getElementById("themeIcon");
+
+  // Load saved theme
+  if (localStorage.getItem("theme") === "dark") {
+    html.classList.add("dark");
+    themeIcon.classList.replace("fa-moon", "fa-sun");
+  }
+
+  function toggleDarkMode() {
+    html.classList.toggle("dark");
+
+    if (html.classList.contains("dark")) {
+      localStorage.setItem("theme", "dark");
+      themeIcon.classList.replace("fa-moon", "fa-sun");
+    } else {
+      localStorage.setItem("theme", "light");
+      themeIcon.classList.replace("fa-sun", "fa-moon");
+    }
+  }
+
+tailwind.config = {
+    darkMode: 'class'
+  }
