6f49006c89713d48511c8c3342089ea6f39d4ed5
Rakhul <rakhulsankar@gmail.com>
2026-01-13 11:34:00 +0530

    added  light

--- script.js
+++ script.js
@@ -30,3 +30,25 @@ document.addEventListener("DOMContentLoaded", () => {
     icon.classList.replace("fa-moon", "fa-sun");
   }
 });
+
+const html = document.documentElement;
+const themeIcon = document.getElementById("themeIcon");
+
+  // Load saved theme
+  if (localStorage.getItem("theme") === "dark") {
+    html.classList.add("dark");
+    themeIcon.classList.replace("fa-moon", "fa-sun");
+  }
+
+  function toggleDarkMode() {
+    html.classList.toggle("dark");
+
+    if (html.classList.contains("dark")) {
+      localStorage.setItem("theme", "dark");
+      themeIcon.classList.replace("fa-moon", "fa-sun");
+    } else {
+      localStorage.setItem("theme", "light");
+      themeIcon.classList.replace("fa-sun", "fa-moon");
+    }
+  }
+
